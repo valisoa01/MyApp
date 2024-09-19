7c44a099a550dd955c8681aa435dbbaff346aaaa
valisoa01 <valisoa.rafanomezantsoa@student.passerellesnumeriques.org>
2024-09-19 14:21:45 +0300

    css

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='val'>
  
       <div>Hello World , my name Valisoa, student of PNm</div>
       <div>Hello World , my name Valisoa, student of PNm</div>
       <div>Hello World my name Valisoa</div>
       <div>Hello Valisoa my name is Eddy</div>
       <div> how to make this happen</div>
+      </div>
  
     </div>
   );
