155c568208866fbabfa0a1151801b74a3c3fd336
valisoa01 <valisoa.rafanomezantsoa@student.passerellesnumeriques.org>
2024-09-19 13:49:49 +0300

    val

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
       <div>Hello World my name Valisoa</div>
       <div>Hello Valisoa my name is Eddy</div>
       <div> how to make this happen</div>
+       
+      <div>Hello World , my name Valisoa, student of PNm</div>
+      <div>Hello World , my name Valisoa, student of PNm</div>
+      <div>Hello World my name Valisoa</div>
+      <div>Hello Valisoa my name is Eddy</div>
+      <div> how to make this happen</div>
  
     </div>
   );
